86d78b4da382eed9b6ce05e3bfff1b091c768f47
yes <yes@emq.com>
2019-11-11 12:12:12 +0800

    wip

--- chrome-extension/src/content_scripts/index.js
+++ chrome-extension/src/content_scripts/index.js
@@ -1,36 +1,34 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-import React, { useState, useEffect } from "react";
-import { render } from "react-dom";
-import osmWaysToGeoJSON from "../osm-ways-to-geojson";
-import Style from "./style";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+import osmWaysToGeoJSON from '../osm-ways-to-geojson';
+import Style from './style';
 
 const App = () => {
   const [ways, setWays] = useState([]);
 
   useEffect(() => {
     const addMapClickListener = () => {
-      const onMapClick = e => {
+      const onMapClick = (e) => {
         const className = e.target.className.baseVal;
-        if (className.indexOf("way") !== -1) {
+        if (className.indexOf('way') !== -1) {
           const way = className
-            .split(" ")
+            .split(' ')
             .pop()
-            .split("-")[0]
-            .replace("w", "");
+            .split('-')[0]
+            .replace('w', '');
           setWays(ways.concat([way]));
         }
       };
-      const map = document
-        .querySelector("#map iframe")
-        .contentDocument.querySelector("#map");
+      const map = document.querySelector('#map iframe').contentDocument.querySelector('#map');
       if (map) {
-        map.addEventListener("click", onMapClick);
+        map.addEventListener('click', onMapClick);
       } else {
         setTimeout(addMapClickListener, 500);
       }
       return () => {
-        map.removeEventListener("click", onMapClick);
+        map.removeEventListener('click', onMapClick);
       };
     };
     addMapClickListener();
@@ -50,11 +48,11 @@ const App = () => {
         </button>
         <button onClick={() => setWays([])}>Clear</button>
       </div>
-      <div>{`ways: ${ways.join(" > ")}`}</div>
+      <div>{`ways: ${ways.join(' > ')}`}</div>
     </Style>
   );
 };
 
-const waysDiv = document.createElement("div");
+const waysDiv = document.createElement('div');
 document.body.append(waysDiv);
 render(<App />, waysDiv);
